a49f137c5f9d97e1c228804f3be26aafc92957e7
Leandro Carriego <leandrocarriego@hotmail.com>
2023-05-17 16:51:40 -0300

    estilos

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Item from '../Item/Item'
+import './ItemList.css'
 
 const ItemList = ({ products }) => {
   return (
-    <div>
+    <div className='items-container'>
       {products.map(product => {
         return <Item key={product.id} {...product} />
       })}
